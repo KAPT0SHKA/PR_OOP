faf9b693681caa777b3cdccb725f7aa768a2ae17
Ростислав <120830522+KAPT0SHKA@users.noreply.github.com>
2024-03-29 11:27:39 +0000

    1

---
+++ Exercise.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.lang.Math;
+
+public class Exercise {
+        static boolean IsSimple(int ANum) {
+          if (ANum < 2){
+            return false;
+          }
+          double s = Math.sqrt(ANum);
+          for (int i = 2; i <= s; i++) {
+            if (ANum % i == 0)
+              return false;
+          }
+          return true;
+        }
+        public static void main(String[] args) {
+            Random rand = new Random();
+            int[] Array = new int[10];
+            for (int i = 0; i < 10; i++) {
+                while(true){
+                    int num = rand.nextInt(100);
+                    if(IsSimple(num) == true){
+                        Array[i] = num;
+                        break;
+                    }
+                }
+                System.out.print(Array[i] + " ");
+            }
+        }
+    }
+
